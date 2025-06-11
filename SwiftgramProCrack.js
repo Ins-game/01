f14ed94e0400cd6961d12cf196a60f4e0102e5eb
Ins-game <111698897+Ins-game@users.noreply.github.com>
2025-06-12 01:06:24 +0800

    Update SwiftgramProCrack.js

--- SwiftgramProCrack.js
+++ SwiftgramProCrack.js
@@ -1,18 +1,3 @@
-/*************************************
-
-> 应用名称：Swiftgram
-> 解锁功能：解锁订阅/会员
-> 作者：Ins-game（根据 Nicegram 改写）
-> 更新时间：2025-06-12
-> ⚠️仅供学习参考，请勿用于商业用途！
-
-[rewrite_local]
-^https?:\/\/api\.swiftgram\.app\/v0\/settings url script-response-body https://raw.githubusercontent.com/Ins-game/01/main/swiftgramProCrack.js
-
-[mitm]
-hostname = api.swiftgram.app
-
-**************************************/
 
 var body = $response.body
   .replace(/"subscription":\w+/g, '"subscription":true')
